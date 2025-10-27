efb36a3d0223d675618f6549d6a0e462db0bc08a
sashankreddy96 <sathisashankreddy@gmail.com>
2025-10-27 14:46:39 +0530

    Add files via upload
    AREA OF SQUARE USING C-PROGRAMING

---
+++ area of square.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int side,area;
+	printf("enter the side of the square");
+	scanf("%d",&side);
+	area=side*side;
+	printf("area of the square=%d",area);
+	return 0;
+}
