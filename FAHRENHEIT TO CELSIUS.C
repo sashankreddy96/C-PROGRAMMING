8de41ad0dbd82e0a57ddd90fc23e8797285d166e
sashankreddy96 <sathisashankreddy@gmail.com>
2025-10-27 16:30:02 +0530

    FAHRENHEIT TO CELSIUS.C
    FAHRENHEIT TO CELSIUS.C

---
+++ FAHRENHEIT TO CELSIUS.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float f, c;
+    printf("enter the value of f
+");
+    scanf("%f", &f);
+    c = (f - 32) / 1.8;
+    printf("c = %f", c);
+    return 0;
+}
