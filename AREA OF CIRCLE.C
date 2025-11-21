ece0727c283b7449503f26e53c0092b04403f22d
sashankreddy96 <sathisashankreddy@gmail.com>
2025-11-21 15:29:30 +0530

    AREA OF CIRCLE.C
    AREA OF CIRCLE.C

--- AREA OF CIRCLE.C
+++ AREA OF CIRCLE.C
@@ -3,8 +3,7 @@
 int main()
 {
     float pi = 3.14, r, area;
-    printf("enter r value
-");
+    printf("enter r value");
     scanf("%f", &r);
     area = pi * r * r;
     printf("area = %f", area);
